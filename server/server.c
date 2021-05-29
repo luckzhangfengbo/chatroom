6c2941635d448ad22cdf7e3569e3f1582187962a
luckzhangfengbo <1819067326@qq.com>
2021-05-29 14:56:04 +0800

    chatroom

--- server/server.c
+++ server/server.c
@@ -43,6 +43,7 @@ int find_sub() {
 bool check_online(char *name) {
     for (int i = 0; i < MAX_CLIENT; i++) {
         if (client[i].online && !strcmp(name, client[i].name)) {
+            printf("D: %s is online\n", name);
             return true;
         }
     }
